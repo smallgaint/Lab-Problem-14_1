d83ec1aaf7d4971c5f43c70eddf43b7e416b6642
s-hong.1 <s-hong.1@onu.edu>
2025-12-11 12:20:26 -0500

    main Complete

---
+++ p14-1/p14-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+double average(double* a, int size);
+double* maximum(double* a, int size);
+double* minimum(double* a, int size);
+
+int main() {
+	int size;
+	cout << "Enter size of array: ";
+	cin >> size;
+	cout << "Enter " << size << " floating-point values: ";
+	double* arr = new double[size];
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+	cout << "Average is " << average(arr, size) << endl;
+	double* maxPtr = maximum(arr, size);
+	cout << "Maximum value is element #" << (maxPtr - arr) << ": " << *maxPtr << endl;
+	double* minPtr = minimum(arr, size);
+	cout << "Minimum value is element #" << (minPtr - arr) << ": " << *minPtr << endl;
+}
+
+
+
